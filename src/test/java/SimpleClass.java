612809db84d3e80416c4d3be6a6f59fe15bfe713
alex <il.alexandru@yahoo.com>
2019-01-07 15:08:50 +0200

    added selenium

--- src/test/java/SimpleClass.java
+++ src/test/java/SimpleClass.java
@@ -1,5 +1,7 @@
 package test.java;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SimpleClass {
@@ -7,6 +9,10 @@ public class SimpleClass {
 
     @Test
     public void simpleTest(){
-        System.out.println("MIAU");
+        System.setProperty("webdriver.chrome.driver", "/Users/alex/IdeaProjects/test/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://altex.ro");
+        driver.manage().window().maximize();
+        driver.quit();
     }
 }
